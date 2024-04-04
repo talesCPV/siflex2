0f7ddd32ee79400389eeb0fe657c3cb384153a2a
talesCPV <tales@flexibus.com.br>
2024-04-04 16:48:04 -0300

    by_script

--- backend/sql.php
+++ backend/sql.php
@@ -22,7 +22,7 @@
         "MAIL-4"  => 'CALL sp_mark_mail(@hash,"x00",x01,x02);', // DATA, ID_FROM, ID_TO
 
         /* FUNCIONÁRIOS */
-        "FUN-0"  => 'CALL sp_view_func(@access,@hash,"x00","x01","x02");', // FIELD,SIGNAL, VALUE
+        "FUN-0"  => 'CALL sp_view_func(@access,@hash,"x00","x01","x02",x03);', // FIELD,SIGNAL, VALUE, ATIVO
         "FUN-1"  => 'CALL sp_set_func(@access,@hash,x00,"x01","x02","x03","x04","x05","x06","x07","x08","x09","x10","x11","x12","x13","x14","x15","x16","x17",x18,"x19");', // id,nome,nasc,rg,cpf,pis,end,num,cidade,bairro,uf,cep,data_adm,data_dem,id_cargo,id_setor,tel,cel,ativo,obs
         "FUN-2"  => 'CALL sp_del_func(@access,@hash,x00);', // ID
 
