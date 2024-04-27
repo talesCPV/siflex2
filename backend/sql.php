879853a1144101513fb682abddd2ab40c577eda5
talesCPV <tales@flexibus.com.br>
2024-04-26 23:48:09 -0300

    by_script

--- backend/sql.php
+++ backend/sql.php
@@ -75,6 +75,7 @@
         "COT-0" => 'CALL sp_view_cotacao(@access,@hash,"x00","x01","x02","x03","x04");', // FIELD,SIGNAL, VALUE, DT_INI, DT_FIN
         "COT-1" => 'CALL sp_set_pedido(@access,@hash,"x00",x01,"x02","x03","x04","x05","x06","x07","x08","x09");', //ID_PED,ID_EMP, DT_PED, DT_ENTREGA,VENDEDOR,COMPRADOR,NUM_PED,DESCONTO,COND_PGTO,OBS
         "COT-2" => 'CALL sp_view_item_cot(@access,@hash,x00);', // ID_PED
+        "COT-3" => 'CALL sp_set_item_ped(@access,@hash,"x00",x01,x02,"x03","x04","x05","x06");', // id,id_prod,id_ped,qtd,valor,und,serv
 
     );
 
